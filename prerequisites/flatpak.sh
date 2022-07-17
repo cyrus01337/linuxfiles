912beffa684477f15f58c833baa2f2bdf54c576f
cyrus01337 <cyrus01337@gmail.com>
2022-07-17 23:30:33 +0100

    Include Flatpak sources

--- prerequisites/flatpak.sh
+++ prerequisites/flatpak.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+for package in $(cat ../sources/flatpak.txt); do
+    flatpak install -y flathub $package
+done
