71e8e0fd9f55da68feedd8e6f80b04df1338fa6b
cyrus01337 <cyrus21337@gmail.com>
2022-07-18 10:28:05 +0100

    Fine-tune with flags

--- zsh.sh
+++ zsh.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-cp -f .zshrc ~
-eval "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
