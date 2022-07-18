6bfdb6aa2b5cdaba713311fffb2cd8197841a13f
cyrus01337 <cyrus21337@gmail.com>
2022-07-18 10:28:49 +0100

    Ensure command order sanity

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+dpkg --add-architecture i386
+sudo apt-fast update
 cat ./sources/repositories.txt | xargs -n 1 add-apt-repository -y
 
 ./apt-fast.sh
-
-cat ./sources/packages.txt | xargs apt-fast install -y
-
-tlp start
+cat ./sources/prerequisites.txt | xargs apt-fast install -y
 
 for script in ./prerequisites/*; do
     $script
