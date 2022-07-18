85e307b9a1ba5042fd8e7c6b7ffefa4c88afd7aa
cyrus01337 <cyrus21337@gmail.com>
2022-07-18 10:29:40 +0100

    Accommodate for prerequisites

--- apt-fast.sh
+++ apt-fast.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-eval "$(curl -sL https://git.io/vokNn)"
+apt-get install -y apt-fast
+
 echo 'Acquire::Languages "none";' >> /etc/apt/apt.conf.d/00aptitude
+echo debconf apt-fast/maxdownloads string 16 | debconf-set-selections
+echo debconf apt-fast/dlflag boolean true | debconf-set-selections
+echo debconf apt-fast/aptmanager string apt-get | debconf-set-selections
--- prerequisites/flatpak.sh
+++ prerequisites/flatpak.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-cat ../sources/flatpak.txt | xargs flatpak install -y flathub
+cat ./sources/flatpak.txt | xargs flatpak install -y flathub
