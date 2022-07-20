8bd7490d1878a77c9ef6e6b2e0998b85e4c923e6
cyrus01337 <cyrus21337@gmail.com>
2022-07-20 09:01:22 +0100

    Add Sublime Text

---
+++ applications/sublime-text.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+apt-fast update 
+apt-fast install -y sublime-text
