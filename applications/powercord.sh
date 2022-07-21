5cdf3d621fea482a837556214ca968f3b750a374
cyrus01337 <cyrus21337@gmail.com>
2022-07-21 05:52:28 +0100

    Replace with custom Powercord setup

--- applications/powercord.sh
+++ applications/powercord.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd ~/Desktop/third-party
-git clone https://github.com/powercord-org/powercord
+git clone https://github.com/cyrus01337/my-powercord-setup.git
 cd powercord
+git submodule update --init --recursive
+git submodule update --remote --recursive
 npm i
 npm run plug
